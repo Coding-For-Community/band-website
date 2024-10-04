de6790f43d94eced5e35bf57d0e6ef72c0c8d98a
Daniel Chen <Daniel_Chen@caryacademy.org>
2024-10-04 10:58:45 -0400

    Added mantine styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import {App} from "./App.tsx";
+import {MantineProvider} from "@mantine/core";
+import {theme} from "./theme.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <MantineProvider theme={theme}>
+          <App />
+      </MantineProvider>
   </StrictMode>
 )
